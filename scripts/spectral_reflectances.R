003f3b7d9d2176db207e992a53dfb78b8bacef2f
o-b-p <s1642155@ed.ac.uk>
2019-10-22 12:32:33 +0100

    Erika added description to the script

---
+++ scripts/spectral_reflectances.R
@@ -0,0 +1,8 @@
+# spectral_reflectances.R
+# 22 October 2019
+# Erika Hodgson
+
+# The purpose of this script is to compare the spectral reflectance of bracken
+# with other plant species. We need to ensure that bracken has a significantly
+# distinguishable spectral signature in order to map it around Loch Tay.
+
